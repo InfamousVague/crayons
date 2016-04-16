342aa982c2661ce49f2d67fad402531969e3654e
wski <contact.mattdylan@gmail.com>
2016-04-16 01:59:41 -0400

    crayons

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # crayons
 Easily console.log colors!
+
+# usage
+
+Simply use dot notation following crayons with any web safe color to quickly console.log in style!
+```javascript
+crayons.DarkGoldenRod('Woo!');
+```
+
+If you're fancy, you can add in things like size, or background color.
+```javascript
+crayons.size(14).DarkGoldenRod('test', 'BlanchedAlmond');
+```
---
+++ crayons.js
@@ -0,0 +1,28 @@
+const allcolors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+class CrayonBox {
+	constructor() {
+  	this.s = 8;
+
+  	this.colorMeBrah = (msg, color, background) => {
+    	if (background) {
+     	 console.log(`%c ${msg}`, `color: ${color}; background: ${background}; font-size: ${this.s}pt;`);
+      } else {
+       console.log(`%c ${msg}`, `color: ${color}; font-size: ${this.s}pt;`);
+			}
+    };
+
+  	allcolors.map(c => {
+    	this[c] = function(msg, background) {
+      	return this.colorMeBrah(msg, c, background);
+      }
+    });
+  }
+
+  size(pt) {
+  	this.s = pt;
+    return this;
+  }
+}
+
+module.exports = new CrayonBox();
